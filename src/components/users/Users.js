c8ecc99bfdbc50cae70fb73688f76c6dac3b4c77
Alexis Villegas Torres <alexisvt@gmail.com>
2020-03-31 18:42:37 -0600

    refact(Users): refactor Users component
    Refactor Users class component to be a functional component insted.

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,39 +1,22 @@
-import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 import UserItem from './UserItem';
 
-export default class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-    ],
-  };
+const Users = ({ users }) => {
+  const userComponents = users.map((user, index) => <UserItem key={index} user={user} />);
 
-  render() {
-    const users = this.state.users.map((user, index) => <UserItem key={index} user={user} />);
-    return <div style={userStyle}>{users}</div>;
-  }
-}
+  return <div style={userStyle}>{userComponents}</div>;
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+};
 
 const userStyle = {
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
   gridGap: '1rem',
 };
+
+export default Users;
