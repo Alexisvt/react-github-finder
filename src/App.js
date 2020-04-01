3f30fe77b8790d43516ee91a3e3d4fa3688f14f7
Alexis Villegas Torres <alexisvt@gmail.com>
2020-03-31 18:43:28 -0600

    feat(App): fetch GitHub API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import './App.css';
 
+import axios from 'axios';
 import React, { Component } from 'react';
 
 import NavBar from './components/layout/NavBar';
 import Users from './components/users/Users';
 
 class App extends Component {
+  state = {
+    loading: false,
+    users: [],
+  };
+  async componentDidMount() {
+    this.setState((state, props) => ({
+      loading: true,
+    }));
+
+    const { data } = await axios.get(`https://api.github.com/users`);
+
+    this.setState((state, props) => ({
+      loading: false,
+      users: data,
+    }));
+  }
+
   render() {
+    console.log('users', this.state.users);
     return (
       <div className="App">
         <NavBar title="Github Finder" />
         <div className="container">
-          <Users />
+          <Users users={this.state.users} />
         </div>
       </div>
     );
